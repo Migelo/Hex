6da3367cfdc5594330ee98ef1ed7f07809de07ea
Migelo <miha@filetki.si>
2016-09-07 16:41:02 +0200

    Revert "Dodaj človeškega igralca"
    This reverts commit ef7d473c4ef3e0b9f0477906ea9859c67c4949d7.

--- hex.py
+++ hex.py
@@ -7,11 +7,6 @@ import argparse
 VELIKOST = 8
 MODRI = 'M'
 RDECI = 'R'
-seznam= []
-
-radij=20.
-premik=radij*.87
-zamik = 50.
 
 #########################################
 
@@ -29,24 +24,12 @@ def nasprotnik(igralec):
 class Igra():
 
 	def __init__(self):
-		self.plosca = [['PRAZNO' for x in range(VELIKOST)] for y in range(VELIKOST)]
+		self.plosca = [['NaN' for x in range(VELIKOST)] for y in range(VELIKOST)]
 		self.naPotezi = MODRI
 
 	def naredi_potezo(self):
 		#TODO: veljavnost poteze
 		self.na_potezi = nasprotnik(self.na_potezi)
-
-	def veljavna_poteza(self, polje):
-        "Metoda preverja ali je dana poteza veljavna."
-        x = polje / 10
-        y = polje % 10
-        if self.plosca[y][x] != PRAZNO:
-            return False
-        else:
-            for (vrstica, stolpec) in self.sosedi(y, x):
-                if self.plosca[vrstica][stolpec] == self.na_potezi:
-                    return True
-            return False
 
 #########################################
 
@@ -55,7 +38,7 @@ class Gui():
 	TAG_FIGURA = 'fig'
 
 	def __init__(self, root):
-		self.plosca = Canvas(root, width=50*(VELIKOST+1), height=(VELIKOST)*2*radij)
+		self.plosca = Canvas(root, width=50*(VELIKOST+1), height=40*(VELIKOST+1))
 		self.plosca.grid(row=0, column=0)
 
 		self.narisiPlosco()
@@ -66,14 +49,13 @@ class Gui():
 
 	def onObjectClick(self, event):                  
 		print('Got object click', event.x, event.y)
-		print(int(event.widget.find_closest(event.x, event.y)[0]))
-		
-		objekt = int(event.widget.find_closest(event.x, event.y)[0])
-		# if self.igra.veljavna_poteza()
-		# self.igraNarediPotezo()
-
+		print(event.widget.find_closest(event.x, event.y))
 
 	def narisiPlosco(self):
+		radij=20.
+		premik=radij*.87
+		zamik = 50.
+
 		x1, y1 = 0, radij
 		x2, y2 = .87*radij, radij/2
 		x3, y3 = .87*radij, -radij/2
@@ -111,19 +93,6 @@ class Gui():
 		root.destroy()
 #########################################
 
-class Clovek():
-     def __init__(self, gui):
-         self.gui = gui
-
-     def igraj(self):
-         pass
-     def prekini(self):
-         pass
-
-     def klik(self, vr, st):
-         self.gui.naredi_potezo(vr, st)
-
-#########################################
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description="Igrica Hex")
